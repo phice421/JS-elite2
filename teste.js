fb48294e20fd978cbe196b6c236aa98f148d7502
Pedro Batista <130232046+phice421@users.noreply.github.com>
2024-02-15 19:35:18 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,26 @@
+var titulo = document.querySelector(".titulo-ganja");
+titulo.textContent = "Lista de Encomendas";
+
+document.querySelector(".total1").textContent
+ = document.querySelector(".valor-unitario1").textContent
+ * document.querySelector(".quantidade1").textContent
+
+ document.querySelector(".total2").textContent
+ = document.querySelector(".valor-unitario2").textContent
+ * document.querySelector(".quantidade2").textContent
+
+ document.querySelector(".total3").textContent
+ = document.querySelector(".valor-unitario3").textContent
+ * document.querySelector(".quantidade3").textContent
+
+ document.querySelector(".total4").textContent
+ = document.querySelector(".valor-unitario4").textContent
+ * document.querySelector(".quantidade4").textContent
+
+ document.querySelector(".total4").textContent
+ = document.querySelector(".valor-unitario4").textContent
+ * document.querySelector(".quantidade4").textContent
+
+ document.querySelector(".total5").textContent
+ = document.querySelector(".valor-unitario5").textContent
+ * document.querySelector(".quantidade5").textContent
